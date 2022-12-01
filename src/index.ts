9cb04379d3807c69d000db240566df39ba33c297
Divyank Aggarwal <divyank.aggarwal@remotestate.com>
2022-12-01 23:04:41 +0530

    Added an interface for webln

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Sphinx from './lib'
+import Sphinx_webln from './lib_webln'
 
 const sphinx = new Sphinx()
+const webLn = new Sphinx_webln()
 
-module.exports = sphinx
+module.exports = {sphinx,webLn};
